a935ee75747f0f81ed82c4221140e5319efe8d80
Jikku Jose <jikku@storybrain.com>
2023-09-07 18:00:11 +0530

    Revert API

--- api/tts.rb
+++ api/tts.rb
@@ -2,8 +2,6 @@ require_relative "../TTS.rb"
 
 Handler = Proc.new do |request, response|
   text = request.query["text"] || "Hello World"
-  sanitized_text = text.gsub(/[\x00-\x1F\x7F-\x9F]/) { |c| "\\x%02X" % c.ord }
-
 
   response.status = 200
   response["Content-Type"] = "text/text; charset=utf-8"
@@ -12,5 +10,5 @@ Handler = Proc.new do |request, response|
   response["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
   response["Access-Control-Allow-Headers"] = "Content-Type"
 
-  response.body = TTS.new(sanitized_text).download_link
+  response.body = TTS.new(text).download_link
 end
