36fa83e43548faa8a4a58368d8922864d09f43ab
Jikku Jose <jikku@storybrain.com>
2023-09-17 15:34:28 +0530

    Update tts api

--- api/tts-v2.rb
+++ api/tts-v2.rb
@@ -1,4 +1,5 @@
 require "uri"
+require "net/http"
 require_relative "../TTS.rb"
 
 Handler = Proc.new do |request, response|
@@ -6,11 +7,23 @@ Handler = Proc.new do |request, response|
   decoded_text = URI.decode_www_form_component(text)
 
   response.status = 200
-  response["Content-Type"] = "text/text; charset=utf-8"
+  response["Content-Type"] = "audio/mpeg"
+  response["Content-Disposition"] = "attachment; filename=\"output.mp3\""
 
   response["Access-Control-Allow-Origin"] = "*"
   response["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
   response["Access-Control-Allow-Headers"] = "Content-Type"
 
-  response.body = TTS.new(decoded_text).download_file
+  download_link = TTS.new(decoded_text).download_link
+  uri = URI(download_link)
+
+  Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+    request = Net::HTTP::Get.new(uri)
+
+    http.request(request) do |http_response|
+      http_response.read_body do |chunk|
+        response.write(chunk)
+      end
+    end
+  end
 end
