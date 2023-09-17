e789f8429769f67fd614353aa0b0ce19ce4611bb
Jikku Jose <jikku@storybrain.com>
2023-09-17 15:43:23 +0530

    Update

--- api/tts-v2.rb
+++ api/tts-v2.rb
@@ -6,24 +6,20 @@ Handler = Proc.new do |request, response|
   text = request.query["text"] || "Hello World"
   decoded_text = URI.decode_www_form_component(text)
 
+  # Download the MP3 file
+  mp3_url = TTS.new(decoded_text).download_link
+  mp3_data = Net::HTTP.get(URI.parse(mp3_url))
+
+  # Set response headers
   response.status = 200
   response["Content-Type"] = "audio/mpeg"
-  response["Content-Disposition"] = "attachment; filename=\"output.mp3\""
-
+  response["Content-Disposition"] = "attachment; filename=\"#{decoded_text}.mp3\""
+  response["Content-Length"] = mp3_data.bytesize
   response["Access-Control-Allow-Origin"] = "*"
   response["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
   response["Access-Control-Allow-Headers"] = "Content-Type"
 
-  download_link = TTS.new(decoded_text).download_link
-  uri = URI(download_link)
-
-  Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
-    request = Net::HTTP::Get.new(uri)
-
-    http.request(request) do |http_response|
-      http_response.read_body do |chunk|
-        response.write(chunk)
-      end
-    end
-  end
+  # Write the MP3 data to the response body
+  response.body = mp3_data
 end
+
